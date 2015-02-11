0b2436a414f4eee15c5ed3f4fb938abef76ad841
Vec <kek@bur.lol>
2015-02-11 18:58:03 +0100

    La databaselogin i config.php

--- config.php
+++ config.php
@@ -4,6 +4,12 @@
  * Class containing configurations/constants for this project.
  */
 final class Config {
+    // Database login info
+    public static $DB_HOST_NAME = "localhost";
+    public static $DB_USER_NAME = "885666";
+    public static $DB_USER_PASSWORD = "56153";
+    public static $DB_DB_NAME = "885666";
+
     // Image upload constants
     public static $UPLOAD_MAX_FILESIZE_BYTES = 10000000;
     public static $UPLOAD_ALLOWED_FILE_TYPES = array("png", "jpg", "jpeg");
--- connect.php
+++ connect.php
@@ -1,5 +1,7 @@
 <?php
-$db=mysqli_connect("localhost","885666","56153","885666");
+require_once 'config.php';
+
+$db=mysqli_connect(Config::$DB_HOST_NAME, Config::$DB_USER_NAME, Config::$DB_USER_PASSWORD, Config::$DB_DB_NAME);
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
