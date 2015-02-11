83269a976a9f7cebe5bc66aa428c9f35b1a4ead0
Vec <kek@bur.lol>
2015-02-11 18:30:09 +0100

    Startet på bildeupload og la til config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Class containing configurations/constants for this project.
+ */
+final class Config {
+    // Image upload constants
+    public static $UPLOAD_MAX_FILESIZE_BYTES = 10000000;
+    public static $UPLOAD_ALLOWED_FILE_TYPES = array("png", "jpg", "jpeg");
+}
