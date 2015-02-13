9f5acca9c7a674bc657ca5033df54fae6edc2290
Nichlas Torgersen <post@nichlas.no>
2015-02-13 15:30:18 +0100

    ajax and php

--- tests/test_studentByClassId.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	require_once('../class/studentsByClass');
-
-	$cc = 'HISTORIE1';
-	$student = new studentsByClass($cc);
---
+++ tests/test_studentsByClass.php
@@ -0,0 +1,11 @@
+<?php
+	if (isset($_POST['cc'])) {
+		require_once '../class/studentsByClass.php';
+		require_once '../connect.php';
+
+		$input = filter_var($_POST['cc'], FILTER_SANITIZE_STRING);//$_POST['cc'];
+
+		$students = new studentsByClass($input, $db);
+
+		echo json_encode(array ('result' => $students->result, 'input' => $input) );
+	}
