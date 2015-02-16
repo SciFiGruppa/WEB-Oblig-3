31fe986835c542452b5796d7b7393a3a2e0adbcb
Nichlas Torgersen <post@nichlas.no>
2015-02-16 17:09:10 +0100

    fuck

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-require_once 'config.php';
+require_once '../config.php';
 
-$db=mysqli_connect(/*Config::$DB_HOST_NAME*/ 'localhost', /*Config::$DB_USER_NAME*/ 'root', /*Config::$DB_USER_PASSWORD*/ '', /*Config::$DB_DB_NAME*/ '885666');
+$db=mysqli_connect(Config::$DB_HOST_NAME,Config::$DB_USER_NAME,Config::$DB_USER_PASSWORD,Config::$DB_DB_NAME);
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
