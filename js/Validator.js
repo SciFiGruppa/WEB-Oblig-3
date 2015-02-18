0ad3d8103ae8de330c6c65f412e3bb50f66c0e58
Vec <hi@jakttider.com>
2015-02-19 00:11:01 +0100

    Started work at Validator

---
+++ js/Validator.js
@@ -0,0 +1,16 @@
+// when BUTTON is clicked
+// find all <input> elements of type text.
+// check that they are not null
+// if they are null set input element's innerhtml til rød bakgrunn eler noe sånt
+// if they are non-null, set background to green.
+
+
+// Make array containing the ID's of <input type="text"> in the HTML object
+var validator = {
+
+    getInputsArray : function() {
+        var inputs = document.body.getElementsByTagName("input"); // get all <input> elements
+    }
+
+};
+
