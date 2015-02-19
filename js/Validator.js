f45ce625fd0dd2286f59f5a6dfb210494dd223aa
Vec <kek@bur.lol>
2015-02-19 01:02:38 +0100

    uferdig work på validator

--- js/Validator.js
+++ js/Validator.js
@@ -7,10 +7,36 @@
 
 // Make array containing the ID's of <input type="text"> in the HTML object
 var validator = {
+    dummyVar: "dummytext",
+    testVar: "testtext",
 
-    getInputsArray : function() {
-        var inputs = document.body.getElementsByTagName("input"); // get all <input> elements
+    getInputElements: function() {
+        // Getting NodeList with HTMLInputObjects
+        var nodeListInputs = document.body.getElementsByTagName("input"); // get all <input> elements
+
+        // Putting only the <input type="text"> elements in an array.
+        var inputsArray = [];
+        for(var i = 0; i < nodeListInputs.length; i++) {
+            if(nodeListInputs[i].type === "text") {
+                inputsArray[i] = nodeListInputs[i];
+            }
+        }
+
+        return inputsArray;
+    },
+
+    getSubmitButton: function() {
+        var nodeListSubmit = document.body.getElementsByTagName("input");
+
+        // Try to find the submit button
+
     }
 
 };
+
+var inputs = validator.getInputElements();
+
+alert(inputs[0]);
+
+
 
