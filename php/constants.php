40f0344f91fd9459ad1e141d249f3d018ed53f7e
Vec <kek@bur.lol>
2015-03-02 13:22:24 +0100

    fjerne min login i constants.php

--- php/constants.php
+++ php/constants.php
@@ -5,12 +5,12 @@
 final class Config {
     // Database login info
     public static $DB_HOST_NAME = "localhost";
-    public static $DB_USER_NAME = "106241";
-    public static $DB_USER_PASSWORD = "65141";
-    public static $DB_DB_NAME = "106241";
+    public static $DB_USER_NAME = ""; // studentnummer
+    public static $DB_USER_PASSWORD = ""; // db passord (samme som phpmyadmin)
+    public static $DB_DB_NAME = ""; // studentnummer
 
     // Image upload constants
-    public static $UPLOAD_IMAGE_PREFIX = "106241"; // Skriv studentnummeret ditt her.
+    public static $UPLOAD_IMAGE_PREFIX = ""; // studentnummer
     public static $UPLOAD_PATH = "D:\\Sites\\home.hbv.no\\phptemp\\";
     public static $UPLOAD_MAX_FILESIZE_BYTES = 10000000; // 10mb
     public static $UPLOAD_MAX_DESCRIPTION_LENGTH = 200; // character length
